72bbfd9c92ef500864db7a4fa9e7c6b55f7d3dbc
Vasu <b_vasudeva_rao@yahoo.com>
2016-11-30 13:04:05 -0600

    Update justTest.js

--- justTest.js
+++ justTest.js
@@ -3,6 +3,8 @@ getForecast({context, entities}) {
     var location = firstEntityValue(entities, "location")
     if (location) {
       context.forecast = 'sunny in ' + location; // we should call a weather API here
+      //api key for open weather api - api.openweathermap.org/data/2.5/weather?q={city name}&APPID=d4fe7bbbb723c3cd52e9d9d614aa819e - d4fe7bbbb723c3cd52e9d9d614aa819e
+      //api.openweathermap.org/data/2.5/forecast/city?id=524901&APPID=d4fe7bbbb723c3cd52e9d9d614aa819e
       delete context.missingLocation;
     } else {
       context.missingLocation = true;
